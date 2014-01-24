5bf1a88a4ae6a008c9c586db8562664fc64bb50f
Benedicte Ofstad <benediof@gmail.com>
2014-01-24 13:00:24 +0100

    Revert "Tracked a new file. Made som rearrangments of the code to object-orient"
    This reverts commit 28bd716d575fa30536eff1775ac3dea21e9608ca.

--- read_input.py
+++ read_input.py
@@ -852,3 +852,36 @@ def read_cubic_force_field_chiral(filename, n_cord): #Make this one generic
     f.close()
     return cubic_force_field
 
+def write_to_file(molecule, property_type, results, n_atom = None):
+    
+        filename = "output/" + molecule
+        f = open(filename, "a")
+        f.write(property_type + "\n")
+        
+        if(results.ndim == 1):
+            line = str(results).strip('[]')
+            f.write(line + "\n")
+            f.close()
+        
+        if(results.ndim == 2):
+            line1 = str(results[0]).strip('[]')
+            line2 = str(results[1]).strip('[]')
+            line3 = str(results[2]).strip('[]')
+            
+            f.write(line1 + "\n")
+            f.write(line2 + "\n")
+            f.write(line3 + "\n")
+
+            f.close()
+
+        if(results.ndim == 3):        
+            for atom in range(n_atom):
+                line1 = str(results[atom][0]).strip('[]')
+                line2 = str(results[atom][1]).strip('[]')
+                line3 = str(results[atom][2]).strip('[]')
+            
+                f.write(line1 + "\n")
+                f.write(line2 + "\n")
+                f.write(line3 + "\n")
+                
+                f.write("\n") # Seperates the 2D matrices making up the 3D matrix
